00e2ed6c6b753be545ea81e2544a83bc6937d0c7
Porndet Pipitsuntonsan <40681502+porndet@users.noreply.github.com>
2025-01-27 22:55:25 +0900

    Add files via upload

--- MegBoxUI.xaml.cs
+++ MegBoxUI.xaml.cs
@@ -1,27 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+﻿using System.Windows;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace WpfApp2__.NET_Framework_
 {
-    /// <summary>
-    /// Interaction logic for MegBoxUI.xaml
-    /// </summary>
     public partial class MegBoxUI : Window
     {
         public MegBoxUI()
         {
             InitializeComponent();
+
+            int type = 2;
+            bool OKCancel = false;
+
+            if(type == 1)
+            {
+                BdIcon.Background = this.FindResource("SuccessColor_Gradient") as GradientBrush;
+                BdText.Background = this.FindResource("SuccessColor_Gradient") as GradientBrush;
+                TitleText.Foreground = this.FindResource("SuccessColor_Text") as Brush;
+                TitleText.Text = "Success";
+                iconImg.Data = this.FindResource("circle-check") as Geometry;
+            }
+            else if(type == 2)
+            {
+                BdIcon.Background = this.FindResource("ErrorColor_Gradient") as GradientBrush;
+                BdText.Background = this.FindResource("ErrorColor_Gradient") as GradientBrush;
+                TitleText.Foreground = this.FindResource("ErrorColor_Text") as Brush;
+                TitleText.Text = "Error";
+                iconImg.Data = this.FindResource("circle-xmark") as Geometry;
+            }
+            else
+            {
+                BdIcon.Background = this.FindResource("QuestionColor_Gradient") as GradientBrush;
+                BdText.Background = this.FindResource("QuestionColor_Gradient") as GradientBrush;
+                TitleText.Foreground = this.FindResource("QuestionColor_Text") as Brush;
+                TitleText.Text = "Question";
+                iconImg.Data = this.FindResource("circle-question") as Geometry;
+            }
+
+            if(!OKCancel)
+            {
+                Btnok.Margin = new Thickness(0, 0, 0, 0);
+                Btncancel.Visibility = Visibility.Collapsed;
+            }
         }
     }
 }
