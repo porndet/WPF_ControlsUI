af04df582d6d00ff31769bfd44aa9d2d76dee772
Porndet Pipitsuntonsan <40681502+porndet@users.noreply.github.com>
2025-03-10 22:27:20 +0900

    Add files via upload

---
+++ GetDataFolderItemNo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2__.NET_Framework_.ViewModel
+{
+    public class GetDataFolderItemNo
+    {
+        public ObservableCollection<FolderItemsDayOfWeeks> ItemsDayOfWeeks { get; set; } =
+                new ObservableCollection<FolderItemsDayOfWeeks>();
+
+        public event EventHandler ChangeItemsDayOfWeek;
+        public GetDataFolderItemNo()
+        {
+            foreach (var item in Directory.GetDirectories(@"48821-3431"))
+            {
+                ItemsDayOfWeeks.Add(new FolderItemsDayOfWeeks()
+                {
+                    CheckedFile = false,
+                    Name = Path.GetFileName(item),
+                });
+
+                ItemsDayOfWeeks.Last().PropertyChanged += GetDataFolderItemNo_PropertyChanged;
+            }
+        }
+
+        private void GetDataFolderItemNo_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            ChangeItemsDayOfWeek?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    public class FolderItemsDayOfWeeks : INotifyPropertyChanged
+    {
+        private bool checkedFile { get; set; }
+        public bool CheckedFile
+        {
+            get { return checkedFile; }
+            set
+            {
+                checkedFile = value;
+                OnPropertyChanged("CheckedFile");
+            }
+        }
+        public string Name { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ Window3.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfApp2__.NET_Framework_.ViewModel;
+
+namespace WpfApp2__.NET_Framework_
+{
+    public partial class Window3 : Window
+    {
+        public Window3()
+        {
+            var GetData = new GetDataFolderItemNo();
+            GetData.ChangeItemsDayOfWeek += GetData_ChangeItemsDayOfWeek;
+            this.DataContext = GetData;
+
+            InitializeComponent();
+        }
+
+        private void GetData_ChangeItemsDayOfWeek(object sender, EventArgs e)
+        {
+            if (sender is GetDataFolderItemNo DataFolder)
+            {
+                foreach(var item in DataFolder.ItemsDayOfWeeks.Where(x => x.CheckedFile))
+                {
+                    Console.WriteLine(item.Name + " " + item.CheckedFile);
+                }
+            }
+        }
+    }
+}
